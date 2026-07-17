f238a1abc84d7b99568641381f870c63c244e865
dakshsinghtech2008 <dakshsingh.tech2008@gmail.com>
2026-07-17 11:01:13 +0530

    Day 05 practic

---
+++ Day05.py
@@ -0,0 +1,6 @@
+# AVARAGE OF THREE NUMBERS AND OUTPUT IN WITHOUT DECIMAL
+x=int(input("ENTER THE FRIST NUMBER : "))
+y=int(input("ENTER THE SECOND NUMBER : "))
+z=int(input("ENTER THE THIRD NUMBER : "))
+Avarage=(x+y+z)/3
+print(round(Avarage))
