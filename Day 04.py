4351befecdd0133d856359bb681bbbc91758899a
dakshsinghtech2008 <dakshsingh.tech2008@gmail.com>
2026-07-17 10:52:25 +0530

    Day 04 python practic

---
+++ Day 04.py
@@ -0,0 +1,9 @@
+#SALARY WITH HRA AND DA
+salary=int(input("ENTER YOUR SALARY : "))
+HRA=salary*40/100
+print("HRA : ",HRA)
+DA=salary*20/100
+print("DA : ",DA)
+Total_salary=salary+HRA+DA
+print("TOTAL SALARY : ",Total_salary)
+
