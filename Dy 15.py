d8f6cd1cd555fd6abd9ca47ed780bab6ba65fae2
dakshsinghtech2008 <dakshsingh.tech2008@gmail.com>
2026-07-23 20:23:11 +0530

    Day 15 python practic

---
+++ Dy 15.py
@@ -0,0 +1,6 @@
+# Write a Python program to input a number from the user and print its multiplication table from 1 to 10 using a while loop.
+x=int(input("ENTER ONE NUMBER : "))
+y=1
+while y<=10:
+    print(x*y)
+    y=y+1
