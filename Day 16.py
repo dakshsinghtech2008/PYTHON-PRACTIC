ba0a08d78ed481939049d8dd7d4a85ceed5cf5dd
dakshsinghtech2008 <dakshsingh.tech2008@gmail.com>
2026-07-23 20:26:56 +0530

    Day 16 python prctic

---
+++ Day 16.py
@@ -0,0 +1,6 @@
+# Write a Python program to input a name from the user and print it 10 times with one line space using a while loop.
+x=input("ENTER 4 NAME :")
+Y=1
+while Y<=10:
+    print('\n' + x)
+    Y=Y+1
