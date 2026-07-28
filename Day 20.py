dec093bcce206535f17489a06276151bedcc17f7
dakshsinghtech2008 <dakshsingh.tech2008@gmail.com>
2026-07-28 07:32:59 +0530

    Day 20 python practic
    Write a Python program using a while loop to input 10 numbers from the user and print the square of each number.

---
+++ Day 20.py
@@ -0,0 +1,7 @@
+#Write a Python program using a while loop to input 10 numbers 
+#from the user and print the square of each number.
+y=1
+while y<=10 :
+    x=int(input("ENTER ANY NUMBER : "))
+    print("SQURE : ",x*x)
+    y=y+1
