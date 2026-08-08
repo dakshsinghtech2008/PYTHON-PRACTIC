faf7ecc2c987a8c15f1eda6675d6d418cd77d24b
dakshsinghtech2008 <dakshsingh.tech2008@gmail.com>
2026-08-08 14:41:49 +0530

    Day 24 python pyactic
    Write a Python program to accept a 5-digit number from the user and print its reverse. If the entered number is not a 5-digit number, display an appropriate message

---
+++ Day 24.py
@@ -0,0 +1,18 @@
+#Write a Python program to accept a 5-digit number from the user and print its reverse. 
+# If the entered number is not a 5-digit number, display an appropriate message
+x = int(input("ENTER 5 DIGIT NUMBER : "))
+y=x
+count = 0
+while y>0:
+    y=y//10
+    count = count + 1
+if count == 5:
+    A=x
+    B= 0
+    while A>0:
+        digit=A % 10
+        B= B*10+digit
+        A= A//10
+    print("REVERSE :", B)
+else:
+    print("PLEASE ENTER ONLY 5 DIGIT NUMBER")
